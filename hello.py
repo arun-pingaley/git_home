72e847ebe1659fd2e408350c32679c5d5814e9a2
Arun Pingaley <arun@pingaley.org>
2017-08-08 22:53:31 +0530

    Added the first file to git

---
+++ hello.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  8 22:29:20 2017
+
+@author: Arun Pingaley
+"""
+
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None"""
+    print("Hello World")
+
+# main program starts here
+hello()
